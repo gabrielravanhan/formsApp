67e8cb2184de4c628ac44f6596470cbd1394199e
gabrielravanhan <gabrielravanhanjose@gmail.com>
2022-11-17 15:02:13 -0300

    ReactiveFormsModule

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +11,11 @@ export class LoginPage implements OnInit {
   mostrarSenha = false;
   iconeSenha = 'eye';
 
-  constructor() { }
+  formLogin = this.formBuilder.group({
+    email: ['']
+  });
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
   }
