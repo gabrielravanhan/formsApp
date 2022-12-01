a5cbb63889c7dad165840b203c4e27eebd313311
Gabriel José Soto Ravanhan <gabrielravanhanjose@gmail.com>
2022-12-01 09:50:53 -0300

    ...

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -11,11 +11,26 @@ export class LoginPage implements OnInit {
   mostrarSenha = false;
   iconeSenha = 'eye';
 
-  formLogin = this.formBuilder.group({
-    email: ['']
-  });
+  formLogin: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  mensagens = {
+    email: [
+      { tipo: 'required', mensagem: 'O campo E-mail é obrigatório.' },
+      { tipo: 'email', mensagem: 'E-mail Inválido.' },
+    ],
+    senha: [
+      { tipo: 'required', mensagem: 'É obrigatório confirmar senha.' },
+      { tipo: 'minlength', mensagem: 'A senha deve ter pelo menos 6 caracteres.', },
+      { tipo: 'maxlength', mensagem: 'A senha deve ter no máximo 8 caracteres.' },
+    ]
+  };
+
+  constructor(private formBuilder: FormBuilder) {
+    this.formLogin = this.formBuilder.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      senha: ['', Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(8)])]
+    });
+  }
 
   ngOnInit() {
   }
